c6b26eea7b7a9e8b5721fafd4bbf703e2553e2d5
Bryan Van de Ven <bryan@bokeh.org>
2021-11-27 17:43:59 -0800

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -13,8 +13,8 @@ module Onebox
       include StandardEmbed
       include LayoutSupport
 
-      REGEX = /^https?:\/\/docs.bokeh.org(?:\/\w+)*\/[\w\-\.]+#([\w\-]+)$/
-      matches_regexp REGEX
+      NARRATIVE_REGEX = /^https?:\/\/docs.bokeh.org\/en\/(?:\w+)\/docs\/(?:dev|user)_guide[\/\w\-\.]+#([\w\-]+)$/
+      matches_regexp NARRATIVE_REGEX
 
       def self.priority
         0
@@ -24,26 +24,29 @@ module Onebox
         "allowlistedgeneric"
       end
 
+      def narrative_data
+        id = url.match(NARRATIVE_REGEX)[1]
+        header = html_doc.xpath("//div[@id='#{id}']/*[self::h1 or self::h2 or self::h3 or self::h4][1]")
+        section = html_doc.xpath("//div[@id='#{id}']/p")
+        html_entities = HTMLEntities.new
+
+        d = { link: link }.merge(raw)
+        d[:title] = html_entities.decode(Onebox::Helpers.truncate(header.text[0...-1], 80))
+        d[:description] = html_entities.decode(Onebox::Helpers.truncate(section.text, 250))
+
+        if !Onebox::Helpers.blank?(d[:site_name])
+          d[:domain] = html_entities.decode(Onebox::Helpers.truncate(d[:site_name], 80))
+        elsif !Onebox::Helpers.blank?(d[:domain])
+          d[:domain] = "http://#{d[:domain]}" unless d[:domain] =~ /^https?:\/\//
+          d[:domain] = URI(d[:domain]).host.to_s.sub(/^www\./, '') rescue nil
+        end
+
+        d
+      end
+
       def data
-        @data ||= begin
-          id = url.match(REGEX)[1]
-          header = html_doc.xpath("//div[@id='#{id}']/h2[1]")
-          section = html_doc.xpath("//div[@id='#{id}']/p")
-
-          html_entities = HTMLEntities.new
-
-          d = { link: link }.merge(raw)
-          d[:title] = html_entities.decode(Onebox::Helpers.truncate(header.text[0...-1], 80))
-          d[:description] = html_entities.decode(Onebox::Helpers.truncate(section.text, 250))
-
-          if !Onebox::Helpers.blank?(d[:site_name])
-            d[:domain] = html_entities.decode(Onebox::Helpers.truncate(d[:site_name], 80))
-          elsif !Onebox::Helpers.blank?(d[:domain])
-            d[:domain] = "http://#{d[:domain]}" unless d[:domain] =~ /^https?:\/\//
-            d[:domain] = URI(d[:domain]).host.to_s.sub(/^www\./, '') rescue nil
-          end
-
-          d
+        if (url =~ NARRATIVE_REGEX)
+          @data ||= narrative_data
         end
       end
 
